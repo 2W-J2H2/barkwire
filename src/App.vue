49bea6a6c9c0ec92991134f31500f2ac329782b8
Jack Hubert <jhubert@2ndwatch.com>
2022-02-14 13:16:19 -0700

    gutted the vue initial build and hardcoded an initial framework for the 'barkwire' application

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <header>
+      <h1>Barkwire</h1>
+    </header>
+    <main>
+      <h2>Dogs</h2>
+      <ul class="dogs">
+        <li>
+          <div class="dog">
+            <h3>Bixby</h3>
+            <p>The best dog</p>
+          </div>
+        </li>
+        <li>
+          <div class="dog">
+            <h3>Bixby</h3>
+            <p>The best dog</p>
+          </div>
+        </li>
+        <li>
+          <div class="dog">
+            <h3>Bixby</h3>
+            <p>The best dog</p>
+          </div>
+        </li>
+      </ul>
+    </main>
   </div>
-  <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style lang="scss"> 
+header, main {
+  padding: 2rem 3rem;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.dogs {
+  padding-left: 0;
+  list-style: none;
+  .dog {
+    margin-bottom: 2rem;
+    padding: 2rem;
+    box-shadow: 0 1px 12px hsl(0, 0%, 80%)
   }
 }
 </style>
