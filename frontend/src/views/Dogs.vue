8e10167e0016f4620fff8b7a3c504e80d870041b
Jack Hubert <jhubert@2ndwatch.com>
2022-02-17 14:45:51 -0700

    application lists dogs dynamically in the Dog vue

--- frontend/src/views/Dogs.vue
+++ frontend/src/views/Dogs.vue
@@ -2,12 +2,39 @@
   <div class="dogs">
     <h2>Dogs</h2>
     <ul>
-      <li class="dog-listing">Bixby - 20</li>
-      <li class="dog-listing">Mesa - 18</li>
-      <li class="dog-listing">Harmony - 3</li>
+      <li
+        v-for="dog in dogs"
+        :key="dog.id"
+        class="dog-listing"
+      >
+        {{dog.name}}} - {{dog.age}}}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      dogs: [
+        {
+          id: 0,
+          name: "Bixby",
+          age: 20,
+        },
+        {
+          id: 1,
+          name: "Mesa",
+          age: 18,
+        },
+        {
+          id: 2,
+          name: "Harmony",
+          age: 4
+        },
+      ]
+    }
+  }
+}
 </script>
